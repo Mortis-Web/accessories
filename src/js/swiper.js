e081c9891ef230d203349384a8a46ce32baa5237
Mortis-Web <memaraaa123@gmail.com>
2025-04-26 19:55:08 +0300

    Add files via upload

---
+++ src/js/swiper.js
@@ -0,0 +1,16 @@
+  const swiper = new Swiper('.mySwiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+
+
